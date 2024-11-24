1356e936ea17d0ddd04228a6e8ffd7a5b229f7fb
iz0xx <zzowich@gmail.com>
2024-11-25 01:22:43 +0300

    updated handlers

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -11,25 +11,36 @@ def openSQL():
 @user_router.message(Command('start'))
 async def cmd_start(msg: types.Message) -> None:
     """Processes the `start` command."""
-    await msg.answer(f'Привет <b>{msg.from_user.first_name}</b>!')
+    conn, cur = openSQL()
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    conn.commit()
+    conn.close()
+    await msg.reply(f'Привет <b>{msg.from_user.first_name}</b>!')
 
 @user_router.message(Command('help'))
 async def cmd_help(msg: types.Message) -> None:
     """Processes the `help` command."""
-    await msg.answer('Мои команды:\n1. /start - запуск бота(приветствие)\n2. /me - твоя личная статистика\n3. /top - топ-5 лидеров беседы по выбитым 777')
+    conn, cur = openSQL()
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    conn.commit()
+    conn.close()
+    await msg.reply('Мои команды:\n1. /start - запуск бота(приветствие)\n2. /me - твоя личная статистика\n3. /top - топ-5 лидеров беседы по выбитым 777')
 
 @user_router.message(Command('me'))
 async def cmd_me(msg: types.Message) -> None:
     conn, cur = openSQL()
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    conn.commit()
     cur.execute('SELECT Win777Count, WinBarCount, WinLemonCount, WinGrapesCount, AllWins, SpinsCount FROM Data WHERE UserID = ?', (msg.from_user.id,))
     fetch_list = cur.fetchone()
+    conn.close()
     wins_777 = fetch_list[0]
     wins_bar = fetch_list[1]
     wins_lemon = fetch_list[2]
     wins_grapes = fetch_list[3]
     wins_all = fetch_list[4]
     spins = fetch_list[5]
-    await msg.answer(f"<b>@{msg.from_user.username}</b>, вот твоя статистика:\n \
+    await msg.reply(f"<b>@{msg.from_user.username}</b>, вот твоя статистика:\n \
 - Выбитых <i>777</i>: {wins_777}\n \
 - Выбитых <i>BAR</i>: {wins_bar}\n \
 - Выбитых <i>Лимончиков</i>: {wins_lemon}\n \
@@ -40,19 +51,20 @@ async def cmd_me(msg: types.Message) -> None:
 
 @user_router.message(Command('top'))
 async def cmd_top(msg: types.Message) -> None:
+    conn, cur = openSQL()
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    conn.commit()
+    conn.close()
     await msg.reply('В разработке...')
 
 @user_router.message()
 async def msg_user(msg: types.Message) -> None:
-    if msg.text:
+    if msg.content_type == 'dice' and msg.dice.emoji == '🎰':
         conn, cur = openSQL()
         cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)', (msg.from_user.id, '@' + msg.from_user.username))
-        cur.execute('SELECT Win777Count FROM Data WHERE UserID = ?', (msg.from_user.id,))
-        cur.execute('SELECT SpinsCount FROM Data WHERE UserID = ?', (msg.from_user.id,))
         cur.execute('UPDATE Data SET SpinsCount = SpinsCount + 1 WHERE UserID = ?', (msg.from_user.id,))
         conn.commit()
         conn.close()
-
         if msg.dice.value == 64:
             conn, cur = openSQL()
             cur.execute('UPDATE Data SET Win777Count = Win777Count + 1 WHERE UserID = ?', (msg.from_user.id,))
@@ -81,7 +93,8 @@ async def msg_user(msg: types.Message) -> None:
             conn.commit()
             conn.close()
             await msg.reply(f'Плюс <i>ЛИМОНЧИК</i> у <b>@{msg.from_user.username}</b>')
-        print(msg.dice.value)
-
-
-
+    elif msg.text:
+        conn, cur = openSQL()
+        cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+        conn.commit()
+        conn.close()
