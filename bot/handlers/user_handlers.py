d374acd5fb809a1a677ff623a31f315d21130113
iz0xx <zzowich@gmail.com>
2024-11-26 00:27:21 +0300

    updated handlers

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -6,22 +6,22 @@ user_router = Router()
 def openSQL():
     conn = sqlite3.connect('dice.db')
     cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS Data (UserID TEXT PRIMARY KEY, Username TEXT, Win777Count INTEGER DEFAULT 0, WinBarCount INTEGER DEFAULT 0, WinLemonCount INTEGER DEFAULT 0, WinGrapesCount INTEGER DEFAULT 0, AllWins INTEGER DEFAULT 0, SpinsCount INTEGER DEFAULT 0)')
+    cur.execute('CREATE TABLE IF NOT EXISTS Data (ChatID TEXT, UserID TEXT, Username TEXT, Win777Count INTEGER DEFAULT 0, WinBarCount INTEGER DEFAULT 0, WinLemonCount INTEGER DEFAULT 0, WinGrapesCount INTEGER DEFAULT 0, AllWins INTEGER DEFAULT 0, SpinsCount INTEGER DEFAULT 0, UNIQUE(ChatID, UserID))')
     return conn, cur
 @user_router.message(Command('start'))
 async def cmd_start(msg: types.Message) -> None:
     """Processes the `start` command."""
     conn, cur = openSQL()
-    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
     conn.commit()
     conn.close()
-    await msg.reply(f'Привет <b>{msg.from_user.first_name}</b>!')
+    await msg.reply(f'Привет <b>{msg.from_user.first_name}</b>! Чтобы играть, отправь эмодзи рулетки 🎰')
 
 @user_router.message(Command('help'))
 async def cmd_help(msg: types.Message) -> None:
     """Processes the `help` command."""
     conn, cur = openSQL()
-    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
     conn.commit()
     conn.close()
     await msg.reply('Мои команды:\n1. /start - запуск бота(приветствие)\n2. /me - твоя личная статистика\n3. /top - топ-5 лидеров беседы по выбитым 777')
@@ -29,9 +29,9 @@ async def cmd_help(msg: types.Message) -> None:
 @user_router.message(Command('me'))
 async def cmd_me(msg: types.Message) -> None:
     conn, cur = openSQL()
-    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
     conn.commit()
-    cur.execute('SELECT Win777Count, WinBarCount, WinLemonCount, WinGrapesCount, AllWins, SpinsCount FROM Data WHERE UserID = ?', (msg.from_user.id,))
+    cur.execute('SELECT Win777Count, WinBarCount, WinLemonCount, WinGrapesCount, AllWins, SpinsCount FROM Data WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
     fetch_list = cur.fetchone()
     conn.close()
     wins_777 = fetch_list[0]
@@ -52,49 +52,66 @@ async def cmd_me(msg: types.Message) -> None:
 @user_router.message(Command('top'))
 async def cmd_top(msg: types.Message) -> None:
     conn, cur = openSQL()
-    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+    cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
     conn.commit()
+    cur.execute('SELECT Win777Count, Username FROM Data WHERE ChatID = ? ORDER BY Win777Count', (msg.chat.id,))
+    fetch_list = cur.fetchall()[:6]
+    top_dict = {}
+    for i in range(len(fetch_list)):
+        top_dict[i + 1] = [fetch_list[i][1], fetch_list[i][0]]
+    output = "Вот топ-5 лидеров по <i>выбитым 777</i> в данном чате:\n\n"
+    for item in top_dict:
+        output += f"{item}. <b>{top_dict[item][0]}</b> выбил 777 {top_dict[item][1]} раз(а)".rstrip() + '\n\n'
     conn.close()
-    await msg.reply('В разработке...')
+    await msg.reply(output)
 
 @user_router.message()
 async def msg_user(msg: types.Message) -> None:
     if msg.content_type == 'dice' and msg.dice.emoji == '🎰':
         conn, cur = openSQL()
-        cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)', (msg.from_user.id, '@' + msg.from_user.username))
-        cur.execute('UPDATE Data SET SpinsCount = SpinsCount + 1 WHERE UserID = ?', (msg.from_user.id,))
+        cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)', (msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
+        conn.commit()
+        cur.execute('UPDATE Data SET SpinsCount = SpinsCount + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
         conn.commit()
         conn.close()
         if msg.dice.value == 64:
             conn, cur = openSQL()
-            cur.execute('UPDATE Data SET Win777Count = Win777Count + 1 WHERE UserID = ?', (msg.from_user.id,))
-            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ?', (msg.from_user.id,))
+            cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
+            conn.commit()
+            cur.execute('UPDATE Data SET Win777Count = Win777Count + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
+            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
             conn.commit()
             conn.close()
             await msg.reply(f'СЮДА! ПЛЮС <i>777</i> У <b>@{msg.from_user.username}</b>')
         elif msg.dice.value == 1:
             conn, cur = openSQL()
-            cur.execute('UPDATE Data SET WinBarCount = WinBarCount + 1 WHERE UserID = ?', (msg.from_user.id,))
-            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ?', (msg.from_user.id,))
+            cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
+            conn.commit()
+            cur.execute('UPDATE Data SET WinBarCount = WinBarCount + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
+            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
             conn.commit()
             conn.close()
             await msg.reply(f'Плюс <i>BAR</i> у <b>@{msg.from_user.username}</b>')
         elif msg.dice.value == 22:
             conn, cur = openSQL()
-            cur.execute('UPDATE Data SET WinGrapesCount = WinGrapesCount + 1 WHERE UserID = ?', (msg.from_user.id,))
-            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ?', (msg.from_user.id,))
+            cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
+            conn.commit()
+            cur.execute('UPDATE Data SET WinGrapesCount = WinGrapesCount + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
+            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
             conn.commit()
             conn.close()
             await msg.reply(f'Плюс <i>ВИНОГРАДИК</i> у <b>@{msg.from_user.username}</b>')
         elif msg.dice.value == 43:
             conn, cur = openSQL()
-            cur.execute('UPDATE Data SET WinLemonCount = WinLemonCount + 1 WHERE UserID = ?', (msg.from_user.id,))
-            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ?', (msg.from_user.id,))
+            cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
+            conn.commit()
+            cur.execute('UPDATE Data SET WinLemonCount = WinLemonCount + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
+            cur.execute('UPDATE Data SET AllWins = AllWins + 1 WHERE UserID = ? AND ChatID = ?', (msg.from_user.id, msg.chat.id))
             conn.commit()
             conn.close()
             await msg.reply(f'Плюс <i>ЛИМОНЧИК</i> у <b>@{msg.from_user.username}</b>')
     elif msg.text:
         conn, cur = openSQL()
-        cur.execute('INSERT OR IGNORE INTO Data (UserID, Username) VALUES (?, ?)',(msg.from_user.id, '@' + msg.from_user.username))
+        cur.execute('INSERT OR IGNORE INTO Data (UserID, Username, ChatID) VALUES (?, ?, ?)',(msg.from_user.id, '@' + msg.from_user.username, msg.chat.id))
         conn.commit()
         conn.close()
